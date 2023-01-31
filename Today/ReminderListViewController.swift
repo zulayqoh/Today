7e26cea0a69a7c3c75e62918e63a0ae27b645918
zulayqoh <topebabs01@gmail.com>
2023-01-30 22:37:09 -0800

    rename the view controller to ReminderListViewController, and change the superclass to UICollectionViewController

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ReminderListViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
