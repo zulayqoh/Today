88d74b82438c2bf606064357fcae6083b62678bd
zulayqoh <topebabs01@gmail.com>
2023-02-01 00:59:38 -0800

    add a dataSource property that implicitly unwraps a DataSource

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ReminderListViewController: UICollectionViewController {
     
     typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
